1e19a9b6678c6b0523c17a0871eeb14dcdc8be0e
lm-afrin <titlee42@gmail.com>
2021-11-23 11:26:13 +0900

    added generic view in views

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    include('api/v1/', include('posts.urls'))
+    path('api/v1/', include('posts.urls'))
 ]
