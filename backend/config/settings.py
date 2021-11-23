2dba68d3bf5dec0018ca13412d4a0e4b58012d13
lm-afrin <titlee42@gmail.com>
2021-11-23 11:03:02 +0900

    add rest framework

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'posts',
 ]
 
@@ -127,3 +128,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': {
+        'rest_framework.permissions.AllowAny'
+    }
+}
