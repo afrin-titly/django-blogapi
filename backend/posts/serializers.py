91d696d6ab8e3b8d84febe243ccac4c2797e0eba
lm-afrin <titlee42@gmail.com>
2021-11-24 21:59:58 +0900

    used viewsets

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 from rest_framework import serializers
 from .models import Post
+from django.contrib.auth import get_user_model
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id', 'author', 'title', 'body', 'created_at')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
